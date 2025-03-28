8f4d10073485be6b42c930badd91c879a974613a
René Uittenbogaard <ruittenb@gmail.com>
2025-03-28 13:38:32 +0100

    Added versions with rivers; added iterations

--- adres.pl
+++ adres.pl
@@ -5,53 +5,52 @@
 # Keppel
 # Soeren
 #
-# Gonsp aan de Donkel
-# Put aan de A
 
+use v5.10.1;
 use strict;
 use POSIX qw(floor);
+
+############################################################################
+# constants
 
 my @CITY_UNIFIXES = qw(Barf Burp Drummel Gonsp Hark Jurk Kimmel Korst Nisk Pik Put Rinsp Smirk);
 
 my @CITY_PREFIXES = qw(
     Aa Aalbesser Acht Achter Beek Blasper Blerker Blinkers Boeren Borrel Casper
     Couwen Daver Duin Eem Eider Even Flens Gaffel Grebben Groenen Grooten Gruns
-    Jilser Heer Heien Hoogen Kaal Kemper Kerk Klammer Kluitje Konkel Kunder Loos
-    Loden Maagden Middel Negen Nijen Over Ouden Poppel Prakke Riet Rood Scharrel
+    Jilser Heer Heien Hoogen Kaal Kemper Kerk Klammer Kloot Kluitje Konkel Kunder
+    Loos Loden Maagden Middel Negen Nijen Over Ouden Poppel Prakke Riet Rood Scharrel
     Schel Scherpen Schoon Tonnen Uiter Valken Voren Water Willems Woud IJ Zeven
     Zijder Zonkel
     Sint-Michiels Sint-Andreas Sint-Anna
 );
 
 my @CITY_SUFFIXES = qw(
-    beek berg broek burg dam dijk dorp drecht heim hoef hoek holt
-    horn hoven huizen kamp kerk kerke land loo oord rade rijk rode sloot
-    sluis stad veen veld vliet voort wijk woude zand zeel zicht zijde zijl
+    beek berg broek burg dam dijk dorp drecht heim hoef hoek holt horn hoven
+    huizen kamp kerk kerke land loo meer oord polder rade rijk rode sloot sluis
+    stad veen veld vliet voort wijk woude zand zeel zicht zijde zijl
 );
 
 my @CITY_EMBELLISHMENTS = qw(Noord Zuid Oost West Oud Nieuw Hoog Laag Groot Klein);
 
-my @RIVERS = qw(A Donkel Kreek Loens);
+my @RIVERS = qw(A Bummel Donkel Kreek Loens);
 
 my @STREET_PREFIXES = (
-    qw(Molen Kerk Stations Klooster Hoofd Ring),
-    qw(Klompen Molenaars Wijngaard),
-    qw(Ruiter),
-    qw(Duin Beekdal Dijkzicht Moeras Vallei),
-    qw(Vlas Riet),
-    qw(Hooiberg Gaffel Ploeg Fuik),
-    qw(Rietveld Beethoven Mozart Mahler Spinoza Sweelinck Mondriaan Liszt),
-    qw(Beatrix Bernhard Juliana),
-    qw(Tollens Newton Einstein Planck),
-    qw(Ouverture Symfonie Aria Concert Viool Harp),
-    qw(Ossen),
-    qw(Vlinder Vuurvlieg Libelle Egel),
-    qw(Sumatra Java Celebes Suriname Curaçao),
-    qw(Dauw Dauwdruppel Zonnegloeds Zonnewende),
-    qw(Agaat Saffier Jade Emerald Onyx),
-    qw(Merel Kievit Havik Nachtegaal Kwartel Zwaluw Valken Reiger),
-    qw(Bloesem Klaver Korenbloem Hortensia Forsythia Distel Braam),
-    qw(Plataan Berken Beuken Eiken Wilgen Hazelaar Dennen Iepen Elzen),
+    qw(Molen Kerk Stations Klooster Hoofd Ring Wijngaard), # city features
+    qw(Duin Beekdal Dijkzicht Moeras Vallei), # landscape features
+    qw(Dauw Dauwdruppel Zonnegloeds Zonnewende), # natural phenomena
+    qw(Hooiberg Gaffel Ploeg Fuik Klompen), # farm features
+    qw(Ruiter Schoenlapper Leerlooier Mandenmaker), # old professions
+    qw(Ouverture Symfonie Aria Concert Viool Harp), # classical music
+    qw(Agaat Saffier Jade Emerald Onyx), # precious stones
+    qw(Sumatra Java Celebes Suriname Aruba Curaçao), # islands
+    qw(Bloesem Klaver Korenbloem Hortensia Forsythia Distel Braam Vlas Riet), # plants
+    qw(Plataan Berken Beuken Eiken Wilgen Hazelaar Dennen Iepen Elzen), # trees
+    qw(Merel Kievit Havik Nachtegaal Kwartel Zwaluw Valken Reiger), # birds
+    qw(Ossen Vlinder Vuurvlieg Libelle Egel Hommel), # animals
+    qw(Beatrix Bernhard Emma Juliana Máxima Nassau Wilhelmina), # royal house
+    qw(Tollens Newton Einstein Planck), # scientists
+    qw(Rietveld Beethoven Mozart Mahler Spinoza Sweelinck Mondriaan Liszt), # composers/painters
 );
 
 my @STREET_SUFFIXES = qw(
@@ -62,8 +61,8 @@ my @STREET_SUFFIXES = qw(
 my @HOUSENUMBER_SUFFIXES = qw(-hs -I -II -III bis A B C D); # ZW RD
 
 ############################################################################
-# functies
-#
+# functions
+
 sub pick {
     my @set = @_;
     return $set[floor(rand() * $#set)]
@@ -75,7 +74,7 @@ sub street {
 
 sub housenumber {
     my $number = floor(2 * exp rand 5);
-    my $suffix = (rand > .9) ? pick(@HOUSENUMBER_SUFFIXES) : '';
+    my $suffix = (rand > .95) ? pick(@HOUSENUMBER_SUFFIXES) : '';
     return $number . $suffix;
 }
 
@@ -83,21 +82,36 @@ sub postcode {
     return floor(rand(9000) + 1000) . " " . chr(65 + rand 26) . chr(65 + rand 26);
 }
 
-sub plaats {
-    if (rand > .15) {
+sub city {
+    if (rand() < .85) {
+        # 85 %
         return pick(@CITY_PREFIXES) . pick(@CITY_SUFFIXES);
-    } elsif (rand > .5) {
+    } elsif (rand() < .333) {
+        # 5 %
+        return pick(@CITY_UNIFIXES) . ' aan de ' . pick(@RIVERS);
+    } elsif (rand() < .5) {
+        # 5 %
         return pick(@CITY_PREFIXES) . pick(@CITY_SUFFIXES) . '-' . pick(@CITY_EMBELLISHMENTS);
     } else {
+        # 5 %
         return pick(@CITY_EMBELLISHMENTS) . '-' . pick(@CITY_PREFIXES) . pick(@CITY_SUFFIXES);
     }
 }
 
+sub output {
+    say street, " ", housenumber;
+    say postcode, " ", city;
+}
+
+sub main {
+    my ($count) = @_;
+    for (1 .. $count || 1) {
+        output;
+    }
+}
+
 ############################################################################
+# main
 
-print street, " ", housenumber, "\n";
-print postcode, " ", plaats, "\n";
-
-
-
+main @ARGV;
 
